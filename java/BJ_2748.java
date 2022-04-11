2cbbe3d7914948c7eb45306eb588b4f99523bbe1
unhas01 <rudtj2966@gmail.com>
2022-04-11 11:36:28 +0900

    2748번

---
+++ java/BJ_2748.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class BJ_2748 {
+
+	static long[] arr;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		arr = new long[N + 1];
+ 
+		for(int i = 0; i < N + 1; i++) {
+			arr[i] = -1;
+		}
+        
+		arr[0] = 0;
+		arr[1] = 1;
+		System.out.println(Fib(N));
+	}
+	
+	public static long Fib(int N) {
+		if(arr[N] == -1) {
+			arr[N] = Fib(N - 1) + Fib(N - 2);
+		}
+		return arr[N];
+	}
+}
