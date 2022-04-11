71210d27d6a0147e4bdfac5837121f7e2d64b469
unhas01 <rudtj2966@gmail.com>
2022-04-11 11:35:20 +0900

    1550번

---
+++ java/BJ_1550.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class BJ_1550 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		System.out.println((int)Integer.parseInt(s, 16));
+	}
+
+}
