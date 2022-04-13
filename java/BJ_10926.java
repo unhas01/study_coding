09f5f293cb639602099f8bd7fdf1391ea350e3a0
unhas01 <rudtj2966@gmail.com>
2022-04-13 20:10:56 +0900

    10926번

---
+++ java/BJ_10926.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class BJ_10926 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		str += "??!";
+		
+		System.out.println(str);
+	}
+
+}
