b6890c3fb961909ec18383b505c8b6fc910e4312
unhas01 <rudtj2966@gmail.com>
2022-04-15 14:40:18 +0900

    11052번

---
+++ java/BJ_11052.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_11052 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int arr [] = new int[N+1];
+		int dp [] = new int[N+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0] = 0;
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+			}
+		}
+		
+		System.out.println(dp[N]);
+		
+	}
+
+}
