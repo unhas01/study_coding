9d0da602b9f7fc7c8786e7c4a0c34fb4c476d132
unhas01 <rudtj2966@gmail.com>
2022-04-15 14:38:52 +0900

    2167번

---
+++ java/BJ_2167.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_2167 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N  = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int arr[][] = new int[N][M];
+
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int K = Integer.parseInt(br.readLine());
+
+		for(int k = 0; k < K; k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			int sum = 0;
+			for(int m = i-1; m < x; m++) {
+				for(int n = j-1;n < y; n++) {
+					sum += arr[m][n];
+				}
+			}
+			System.out.println(sum);
+		
+		
+		
+		}
+
+	}
+}
