86f20f8cbbd5a01ff095a8cb029c07bee3e2ba0e
unhas01 <rudtj2966@gmail.com>
2022-04-17 11:41:20 +0900

    2446번

---
+++ java/BJ_2446.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class BJ_2446 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N ; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}	 
+			for (int k = 0; k < (2 * N - 1) - (2 * i); k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		for (int i = 0; i < N - 1; i++) {
+	        
+			for (int j = 1; j < (N - 1) - i; j++) {	
+				System.out.print(" ");
+			}
+ 
+			for (int k = 0; k < 3 + 2 * i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
