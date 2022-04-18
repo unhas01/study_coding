9810a185b5afd686c9f6bcf69332183cc10af471
unhas01 <rudtj2966@gmail.com>
2022-04-19 00:13:45 +0900

    2193번

---
+++ java/BJ_2193.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class BJ_2193 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		long dp [] = new long[N + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i = 2; i <= N; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}
