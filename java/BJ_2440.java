7648e9358bdf2c36b502ed78f774ddcb49fd7ddc
unhas01 <rudtj2966@gmail.com>
2022-04-20 11:21:07 +0900

    2440번

---
+++ java/BJ_2440.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class BJ_2440 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = N; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
