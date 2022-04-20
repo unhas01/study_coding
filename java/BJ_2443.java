95d3c54030e1715edd86802f73d0ffa27ddb559e
unhas01 <rudtj2966@gmail.com>
2022-04-20 11:22:34 +0900

    2443번

---
+++ java/BJ_2443.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BJ_2443 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j <= N-i; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < N-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		input.close();
+	}
+
+}
