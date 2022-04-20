3698e59db6f758ffe1e5c02b5d1781fec7e4f6a9
unhas01 <rudtj2966@gmail.com>
2022-04-20 11:23:50 +0900

    2445번

---
+++ java/BJ_2445.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BJ_2445 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int N = input.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < N - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < N - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = 0; j < N - i; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < N - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+			input.close();
+		}
+	}
+
+}
