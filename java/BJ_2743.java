9ca952dee55f6db455405cf3e7372b22c1faf83e
unhas01 <rudtj2966@gmail.com>
2022-04-22 18:07:21 +0900

    2743번

---
+++ java/BJ_2743.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class BJ_2743 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		System.out.println(s.length());
+	}
+
+}
