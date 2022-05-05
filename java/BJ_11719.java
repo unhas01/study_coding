f98102e78921024e88dad751ad7a10a5111d04ae
unhas01 <rudtj2966@gmail.com>
2022-05-05 13:37:57 +0900

    11719번

---
+++ java/BJ_11719.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class BJ_11719 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str;
+		
+		while((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+		
+		
+	}
+
+}
