f1ff8061916cdf9fac3eec8a00c8c009235fb9a6
박경서 <rudtj2966@gmail.com>
2022-05-26 20:51:41 +0900

    10824번

---
+++ java/BJ_10824.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_10824 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str [] = br.readLine().split(" ");
+		
+		long n1 = Long.parseLong(str[0] + str[1]);
+		long n2 = Long.parseLong(str[2] + str[3]);
+		
+		System.out.println(n1 + n2);
+	}
+
+}
