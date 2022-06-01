e2218c66415bb7222cccc602b59404d89ff6489f
박경서 <rudtj2966@gmail.com>
2022-06-01 21:06:57 +0900

    5338번

---
+++ java/BJ_5338.java
@@ -0,0 +1,13 @@
+
+public class BJ_5338 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
