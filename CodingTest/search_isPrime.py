472341173a8736d9f7c3d544a89b6e0d8a9d0538
unhas01 <rudtj2966@gmail.com>
2022-06-02 17:11:44 +0900

    소수 찾기

---
+++ CodingTest/search_isPrime.py
@@ -0,0 +1,19 @@
+import math
+
+# 에라토스테네스의 체 사용
+def solution(number):
+    numbers = [True] * (number + 1)
+    answer = 0
+
+    for num in range(2, int(math.sqrt(number)) + 1):
+        if numbers[num] == False:
+            continue
+
+        for i in range(num + num, number + 1, num):
+            numbers[i] = False
+
+    for i in range(2, number + 1):
+        if numbers[i] == True:
+            answer+=1
+
+    return answer
