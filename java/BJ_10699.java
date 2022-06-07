3d765e0d06946f08af9b0cc747c0121ab2742e02
박경서 <rudtj2966@gmail.com>
2022-06-07 22:18:41 +0900

    10699번

---
+++ java/BJ_10699.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BJ_10699 {
+
+	public static void main(String[] args) {
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = new Date();
+		System.out.println(format.format(d));
+	}
+
+}
