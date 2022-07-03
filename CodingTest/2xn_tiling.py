15d3140988534145474899d0dadd556d3848db6a
unhas01 <rudtj2966@gmail.com>
2022-07-03 17:18:31 +0900

    2 x n 타일링

---
+++ CodingTest/2xn_tiling.py
@@ -0,0 +1,12 @@
+def solution(n):
+    dp = [0] * 60001
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, 60001):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+    return dp[n]
+
+
+
+print(solution(4))
