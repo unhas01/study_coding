a55e0200a7186641990fbd1861ea3e097f62597a
unhas01 <rudtj2966@gmail.com>
2022-07-15 19:17:21 +0900

    11650번

---
+++ python/BJ_11650.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+li = []
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    li.append((a, b))
+
+li.sort()
+
+for i in li:
+    print(*i)
