5f477ca6eb7a4e57643d5bc327c39a4b8e0ecea2
unhas01 <rudtj2966@gmail.com>
2022-07-15 19:24:41 +0900

    11651번

---
+++ python/BJ_11651.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+li = []
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    li.append((a, b))
+
+li.sort(key=lambda x:(x[1], x[0]))
+
+for i in li:
+    print(*i)
