b21eed4ec7da54dcad33d936aa3535409577ded5
unhas01 <rudtj2966@gmail.com>
2022-07-16 16:57:50 +0900

    11652번

---
+++ python/BJ_11652.py
@@ -0,0 +1,13 @@
+n = int(input())
+card_dic = {}
+
+for i in range(n) :
+    card = int(input())
+    if card in card_dic :
+        card_dic[card] += 1
+    else :
+        card_dic[card] = 1
+
+result = sorted(card_dic.items(),key = lambda x : (-x[1],x[0]))
+
+print(result[0][0])
